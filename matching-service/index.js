c1d61e051326a48a55a2b273d1e29efcd396a906
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-08-28 21:30:07 +0800

    Coding style

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World from matching-service');
+    // console.log('hello from port 8001')
 });
 
-const httpServer = createServer(app)
+io.on('connection', () => {
+    console.log('connected');
+});
 
-httpServer.listen(8001);
+httpServer.listen(8001, () => {
+    console.log('listening at port 8001');
+});
